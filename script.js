c58d3ae4351402879f6ea9c308a44c7494aa4e12
Chandra Lama <iamme24cl@gmail.com>
2021-03-17 22:29:34 -0700

    implemented a fetch call to backend api to get all transactions, veried and tested returned data

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello from Script.js");
+const expenseApi = 'http://localhost:3000/api/v1/accounts/1/transactions';
+
+// Fetch transactions from api
+function getTransactions() {
+  fetch(expenseApi)
+    .then(response => response.json())
+    .then(json => {
+      json.data.forEach(transaction => {
+        console.log(transaction);
+        addTransactionsToDOM(transaction);
+      });
+    });
+}
+
+// Add Transactions to DOM
+function addTransactionsToDOM(transaction) {
+  
+}
+
+
+
+// Event Listeners
+// Get transactions upon DOM load
+document.addEventListener('DOMContentLoaded', () => {
+  getTransactions();
+});
